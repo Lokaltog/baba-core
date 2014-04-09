ca4e77ef68468553dbb6e4e9164bcf1db85a8ef4
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-04-09 08:19:05 +0200

    Duplicate dada-engine grammar

--- gg.js
+++ gg.js
@@ -128,36 +128,58 @@ var filters = {
 }
 
 var grammar = {
+	// sentences
+	sentence: '$statement#uppercaseFirst.',
+	paragraph: ref('sentence', 2, 5),
+
 	// raw word lists
 	verb: 'add,allot,annotate,apply,archive,bisect,blame,branch,bundle,check,checkout,cherry-pick,clean,clone,commit,configure,count,describe,diff,export,fail,fast-export,fast-import,fetch,filter-branch,format-patch,forward-port,fsck,grep,help,import,index,init,log,merge,name,note,pack,parse,patch,perform,prevent,prune,pull,push,quiltimport,reapply,rebase,reflog,relink,remote,remove,repack,request,reset,reset,return,rev-list,rev-parse,revert,save,send,set,show,specify,stage,stash,strip,succeed'.split(','),
+	verbPerform: 'perform,execute,apply'.split(','),
+	object: 'archive,area,base,branch,change,commit,file,head,history,index,log,object,pack,ref,stage,stash,submodule,subtree,tag,tip,tree'.split(','),
 	location: group(
 		'[non-$verb#verbPresentParticiplify |]',
 		'applied,downstream,local,remote,staged,unstaged,upstream'.split(',')
 	),
-	item: 'archive,stash'.split(','),
-	locatedItem: '[$location |]$item',
-	determiner: 'a few,all,any,some,the,various'.split(','),
-	adjective: 'automatic,passive,temporary,original'.split(','),
 	preposition: 'before,below,for,from,inside,next to,opposite of,outside,over,to'.split(','),
+	determiner: 'a few,all,any,some,the,various'.split(','),
+	chanceDeterminer: 'certain,small,rare'.split(','),
+	adjective: 'automatic,passive,temporary,original'.split(','),
+	conditionConjunction: 'if,when,in case'.split(','),
+	subject: 'you,the user,the $commandName command'.split(','),
 
-	// generic sentences
-	sentence: '$statement#uppercaseFirst.',
-	paragraph: ref('sentence', 2, 5),
+	// make sure we're not consistent
+	wordFailure: 'failure,error,breakdown,segfault,collapse'.split(','),
+	wordPossible: 'possible,a [$chanceDeterminer |]possibility,a [$chanceDeterminer |] chance'.split(','),
+	wordPreviously: 'previously,earlier,formerly,once'.split(','),
+	wordShould: 'will have to,may have to,should'.split(','),
+	wordSometimes: 'sometimes,in some cases'.split(','),
+	wordSupplied: 'supplied,specified,set,defined,provided'.split(','),
 
-	// grammar-specific words and sentences
-	taggedItem: [
-		group('<', ['[new|old]$item', '$verb-$item'], '>'),
+	// grammar objects
+	commandName: 'git-$verb-$object',
+	commandOption: '--[$verb-|]$verb-$object',
+	action: '$verb [$locatedObject#prependAn|$determiner $locatedObject#pluralize] $preposition $verb#verbPastTensify $locatedObject#pluralize',
+	multipleObjects: '$determiner $adjective $object#pluralize',
+	locatedObject: '[$location |]$object',
+	constantObject: [
+		group('<', ['[new|old]$object', '$verb-$object'], '>'),
 		'$verb#uppercase_$verb#uppercase',
 	],
-	multipleItems: '$determiner $adjective $item#pluralize',
-	commandName: 'git-$verb-$item',
-
-	// action (generated description)
-	action: '$verb [$locatedItem#prependAn|$determiner $locatedItem#pluralize] $preposition $verb#verbPastTensify $locatedItem#pluralize',
+	condition: [
+		'$constantObject is [not |]$verb#verbPastTensify',
+	],
 
 	// statements that use all the stuff above
 	statement: [
-		['$multipleItems are $verb#verbPastTensify to $taggedItem by $commandName'],
+		'$conditionConjunction $condition, $commandName $commandOption will $verbPerform $adjective#prependAn $commandName before [$verb#verbPresentParticiplify the $object|doing anything else]',
+		'$multipleObjects $verb#verbPastTensify by $object#pluralize in the $locatedObject but that [$wordSometimes |]are [not |]in $constantObject are $verb#verbPastTensify in $adjective#prependAn $object',
+		'$multipleObjects are $verb#verbPastTensify to $constantObject by $commandName',
+		'the same set of $object#pluralize would [$wordSometimes |]be $verb#verbPastTensify in $adjective#prependAn $object',
+		'$constantObject is $verb#verbPastTensify to $verb the $object of the $object $preposition the $verb',
+		'$multipleObjects that were $wordPreviously $verb#verbPastTensify $preposition the $adjective $object are $verb#verbPastTensify to $adjective#prependAn $object',
+		'it is [$wordSometimes |]$wordPossible that $verb#verbPastTensify#prependAn $wordFailure will prevent $adjective $verb#verbPresentParticiplify of $multipleObjects',
+		'$subject will $verb any such $wordFailure#pluralize and run $commandName $commandOption instead',
+		'to $verb $adjective#prependAn $constantObject and $verb the working $object#pluralize, use the command $commandName $commandOption instead',
 	],
 }
 
