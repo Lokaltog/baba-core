5c2339b214443e56f0d6a1067e41535fde475b1c
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-04-11 11:04:50 +0200

    Accept multiple transform objects in init()

--- baba.js
+++ baba.js
@@ -36,6 +36,25 @@
 		]
 	}
 
+	function mergeRecursive(obj1, obj2) {
+		for (var p in obj2) {
+			try {
+				// Property in destination object set update its value.
+				if (obj2[p].constructor == Object) {
+					obj1[p] = mergeRecursive(obj1[p], obj2[p])
+				}
+				else {
+					obj1[p] = obj2[p]
+				}
+			}
+			catch(e) {
+				// Property in destination object not set create it and set its value.
+				obj1[p] = obj2[p]
+			}
+		}
+		return obj1
+	}
+
 	function randItem (items) {
 		return items[Math.floor(Math.random() * items.length)]
 	}
@@ -198,9 +217,14 @@
 			baba.altTags = altTags
 		}
 		baba.grammar = grammar
-		for (var attr in transforms) {
-			baba.transforms[attr] = transforms[attr]
+
+		// Load all transforms
+		if (!Array.isArray(transforms)) {
+			transforms = [transforms]
 		}
+		transforms.forEach(function (t) {
+			baba.transforms = mergeRecursive(baba.transforms, t)
+		})
 	}
 
 	baba.render = function (template) {
