81d2ffa7c1ceec13919d00efb692715b6ef333da
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-04-11 16:25:38 +0200

    Use === for checking constructor type

--- baba.js
+++ baba.js
@@ -27,7 +27,7 @@
 		for (var p in obj2) {
 			try {
 				// Property in destination object set update its value.
-				if (obj2[p].constructor == Object) {
+				if (obj2[p].constructor === Object) {
 					obj1[p] = mergeRecursive(obj1[p], obj2[p])
 				}
 				else {
