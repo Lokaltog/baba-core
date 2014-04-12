8fb5e19a50cf3d431e2ea5a611ef7d7bd6cc2b4a
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-04-12 20:38:21 +0200

    tumblr grammar: Update grammar
    The grammar now includes everything from the argument generator (plus
    some more), with the exception of sentences/statements and blog-related
    stuff like revolutions (title).

--- src/grammar/tumblr.js
+++ src/grammar/tumblr.js
@@ -38,6 +38,12 @@
 					'good': split('good|great|awesome'),
 					'important': split('important|crucial'),
 				},
+				'phobic': function (variables, grammar) {
+					return grammar['phobia-pre'].map(function (el) {
+						return el + 'phobic'
+					})
+				},
+				'politics': split('anti-SJW|conservative|democrat|dyke|freedom fighter|freegan|liberal|libertarian|misanthrope|pro-choice|pro-life|republican|zapatista'),
 			},
 			'adverb': {
 				'sexual': '{pre.sexual}{post.sexual}',
@@ -51,7 +57,7 @@
 			'verb': {
 				// Must work in any verb.tense: "you {stare-raped} that CAFAB", "{dehumanizing} species is bad"
 				'good': split('abolish|self-diagnose|love'),
-				'bad': split('abuse|attack|criticize|dehumanize|deny|desexualize|discriminate|erase|exotify|exploit|fetishize|harass|hypersexualize|ignore|kinkshame|label|marginalize|misgender|objectify|oppress|reject|sexualize|shame|stare-rape|stigmatize|internalize|institutionalize|colonize|appropriate|erode|censor|blame|condemn|denounce|hate|dominate'),
+				'bad': split('abuse|attack|criticize|dehumanize|deny|desexualize|discriminate|erase|exotify|exploit|fetishize|harass|hypersexualize|ignore|kinkshame|label|marginalize|misgender|objectify|oppress|reject|sexualize|shame|stare-rape|stigmatize|internalize|institutionalize|colonize|appropriate|erode|censor|blame|condemn|denounce|hate|dominate|glorify|rape'),
 				'auxiliary': {
 					'should': split('should|must|need to|can'),
 				},
@@ -76,22 +82,22 @@
 					// Basically converted verbs from verb.bad, i.e. dehumanize -> dehumanization
 					// There are no consistent rules for converting a verb to a noun with a suffix like -ion
 					'good': split('abolishing|self-diagnosing|love'),
-					'bad': split('abuse|attacking|criticization|dehumanization|denial|desexualizing|discrimination|erasure|exotification|exploitation|fetishization|harassment|hypersexualization|ignoring|kinkshaming|labeling|marginalization|misgendering|objectifying|oppression|rejection|sexualization|shaming|stare-raping|stigmatization|internalization|institutionalization|colonization|appropriation|erosion|censoring|blaming|condemnation|denouncement|hate|domination' +
+					'bad': split('abuse|attacking|criticism|dehumanization|denial|desexualizing|discrimination|erasure|exotification|exploitation|fetishization|harassment|hypersexualization|ignoring|kinkshaming|labeling|marginalization|misgendering|objectification|oppression|rejection|sexualization|shaming|stare-raping|stigmatization|internalization|institutionalization|colonization|appropriation|erosion|censoring|blaming|condemnation|denouncement|hate|domination|glorification' +
 					             // extras not in verb.bad
 					             '|entitlement|privilege|culture|domination'),
 				},
 				'abstract': {
 					// Example: "{misandry} is good", "{weight} is good", "{misogyny} is bad"
 					// Will not be pluralized
-					'good': split('misandry|femininity|integrity|equality|intersectionality' +
+					'good': split('misandry|femininity|integrity|equality|intersectionality|multiplicity|ethnicity' +
 					              '|appearance|height|weight|race|womyn|hair|body hair|body|fandom|species|fat|fatty|female|food|stretchmark|color|body image'),
 					'bad': split('masculinity|kyriarchy|patriarcy|superiority|misogyny' +
 					             '|ideals'),
 				},
 				'ism-pre': {
 					// Must be able to be suffixed with -ism and -ist
-					'good-base': split('misandr|femin|equal|intersectional|activ|separat|commun|egalitarian|fandom|fat|lesbian|freegan|social|vegan|vegetarian|athe|food'),
-					'bad-base': split('patriarch|kyriarch|masculin|misogyn|rac|fasc|able|age|binar|assimilation|chauvin|carn|cissex|class|essential|rape-apolog|singlet|traditional|transmisogyn'),
+					'good-base': split('misandr|femin|equal|intersectional|activ|separat|commun|egalitarian|fandom|fat|lesbian|freegan|social|vegan|vegetarian|athe|food|liberation'),
+					'bad-base': split('TERF|patriarch|kyriarch|masculin|misogyn|rac|fasc|able|age|binar|assimilation|chauvin|carn|cissex|class|essential|rape-apolog|singlet|traditional|transmisogyn'),
 					'good': ['{noun.ism-pre.good-base}', 'anti-{noun.ism-pre.bad-base}'],
 					'bad': ['{noun.ism-pre.bad-base}', 'anti-{noun.ism-pre.good-base}'],
 				},
@@ -103,31 +109,88 @@
 					'good': '{noun.ism-pre.good}ist',
 					'bad': '{noun.ism-pre.bad}ist',
 				},
-				'kin-type': split('cat|demon|dog|dolphin|dragon|fox|goat|other|poly|rabbit|wolf'),
-				'kin': '{noun.kin-type}kin',
+				'kin-type': split('cat|demon|dog|dolphin|dragon|fox|goat|other|poly|bunny|wolf|vampire'),
+				'kin': '{noun.kin-type->kin}kin', // the $kin variable is later available, and is also used by pronoun.random
 				'personality': split('individual|personality|person|spirit|entity'),
 				'alignment': '{noun.personality}-[aligned|associating|identifying|type|supporting] {noun.oppressed}',
 				'trigger': '{noun.good} {noun.concept.bad}',
 			},
 			'pronoun': {
-				'subject': split('*e|ey|tho|hu|thon|jee|ve|xe|ze|zhe'), // he
-				'object': split('h*|em|thong|hum|thon|jem|ver|xem|zir|hir|mer|zhim'), // him
-				'possessive': split('h*s|eir|thors|hus|thons|jeir|vis|xyr|zes|hir|zer|zher'), // his
-				'weird-pre': split('bun|cub|fluff|ham|kit|pan|pup|squeak|squid|purr|paw'), // for prefixing like bun/buns/bunself
-				'weird-list': '{pronoun.weird-pre->p}/{$p}s/{$p}self',
-				'second-person': split('you|you all'),
+				// Actually map kins to proper pronouns
+				'random': function (variables) {
+					// Thanks for the help, askanonbinary
+					// http://askanonbinary.tumblr.com/creature
+					var kinPronounMap = {
+						cat: 'purr/purrs/purrself',
+						demon: 'mon/mons/monself',
+						dog: 'pup/pups/pupself',
+						dolphin: 'squeak/squeaks/squeakself',
+						dragon: 'drago/dragons/dragonself',
+						fox: 'fluff/fluffs/fluffself',
+						goat: 'hoof/hooves/hooveself',
+						bunny: 'bun/buns/bunself',
+						wolf: 'wer/weres/wereself',
+						vampire: 'vam/vamps/vampself',
+					}
+					var realPronouns = [
+						'*e/h*s/h*self',
+						'ae/aers/aerself',
+						'ce/cirs/cirself',
+						'e/eir/emself',
+						'fey/fers/ferself',
+						'fir/firs/firself',
+						'he/his/himself',
+						'hir/hirs/hirself',
+						'hen/henom/hens',
+						'jhey/jheir/jheirself',
+						'per/pers/perself',
+						'lee/lis/limself',
+						'she/hers/herself',
+						'thon/thons/thonself',
+						've/virs/virself',
+						'xe/xer/xerself',
+						'zhe/zhim/zherself',
+					]
+					if (kinPronounMap.hasOwnProperty(variables['kin'])) {
+						return kinPronounMap[variables['kin']]
+					}
+					return realPronouns[Math.floor(Math.random() * realPronouns.length)]
+				},
+				'second-person': '[all ]you',
 			},
 			'concept': {
 				'good': [
 					// e.g. ladytype standards, basement dweller rights
 					'{noun.subject.good} {noun.concept.good}',
 					'{noun.subject.bad} {noun.concept.bad}',
-				],
+					'{noun.abstract.bad} {noun.action.bad}',
+				].concat(
+					split('bodily integrity|diversity|food addiction|multiple systems|social justice|female self esteem')
+				).concat([
+					'[self-diagnosed] [racial|gender|species] dysphoria',
+					'fat[ty] love',
+					'[gender|species|race][ abolition| neutrality|-neutral pronouns]',
+					'[colored|non-white|non-western] culture',
+				]).concat(
+					split('anti-porn|body positivity|environmental|fat rights|gay rights|gay|lesbian|social justice|trans*|animal rights|non-white rights').map(function (el) { return el + ' {noun.ism.good}' })
+				),
 				'bad': [
 					// e.g. ladytype standards, basement dweller rights
 					'{noun.subject.good} {noun.concept.bad}',
 					'{noun.subject.bad} {noun.concept.good}',
-				],
+					'{noun.abstract.good} {noun.action.bad}',
+					// institutions, e.g. "institutionalized racism"
+					'[institutionalized|internalized] [gender roles|masculinity|misogyny|patriarchy|racism]',
+					// supremacies/cultures, e.g. "cishet domination"
+					'{noun.subject.bad} [culture|domination|entitlement|feminism|kyriarchy|opinions|privilege|rights|superiority|supremacy]',
+				].concat(
+					split('bindi wearing|hypermasculinity|men\'s rights|patriarchal beauty standards')
+				),
+				'phobia': function (variables, grammar) {
+					return grammar['phobia-pre'].map(function (el) {
+						return el + 'phobia'
+					})
+				},
 			},
 			'fuck': '{verb.swear}',
 			'fuck-off': {
@@ -150,6 +213,7 @@
 					'make love to yourself in a furnace',
 				],
 			},
+			'phobia-pre': split('bi|curvy|deathfat|ethno|fat|femme|furry|hetero|homo|lesbo|male|kin|phallo|poly|queer|trans*|womyn'),
 			'tw-concept': '{noun.oppressed} {noun.concept.bad}',
 			'tw': [
 				'trigger warning: {tw-concept}, {tw-concept}, {tw-concept}',
@@ -175,43 +239,54 @@
 			'statement': {
 				'universal': [
 					'{pronoun.second-person} [fucking ]{noun.subject.good}-{verb.bad|tense.present-participle} {noun.ist.bad|plural} can all {fuck-off.universal}',
-					'[learn|use] [my|the correct] [fucking ]pronouns you [fucking ]{adjective.bad} {noun.subject.bad}, I am {adjective.good|prepend-an}[, {adjective.good}] {noun.kin} and my pronouns are {pronoun.weird-list}',
-					'[like ]seriously, {fuck-off.universal}',
+					'[learn|use] [my|the correct] [fucking ]pronouns you [fucking ]{adjective.bad} {noun.subject.bad}, I am {adjective.good|prepend-an}[, {adjective.good}] {noun.kin} and my pronouns are {pronoun.random}',
 				],
 				'conditional': [
 					'if you XXX',
 				],
 				'toucan': [
-					'never reblog my posts [ever ]again',
+					'[do not|never] reblog my posts [ever ]again',
 					'feel free to unfollow {conjunction.and-or} block/ignore me',
 					'fucking {noun.subject.bad}-{adjective.bad} people',
 					'i [don|can]\'t even',
 					'i am [literally ]100% done',
 					'now go [the fuck ]away',
-					'please leave me alone',
+					'[please ]leave me [the fuck ]alone',
 					'try again, [fucking ]{nouns.privileged}',
-					'unfollow me right now',
+					'unfollow me right [fucking ]now',
 					'you guys are [fucking ]impossible',
 					'for the love of god',
 					'i do not [fucking ]care anymore',
 					'no. just no',
 					'[oh my god|omg]',
-					'just... stop',
+					'just[...] stop',
 					'seriously',
 					'this. is. not. okay',
 					'wow. just. wow',
 					'i do not give a [shit|fuck]',
 					'you know what? [fuck|screw] it',
+					'you did not just [fucking ]say that',
+					'[like ][fucking ]seriously, [just ]{fuck-off.universal}',
+					'i am [literally ]not [fucking ]speaking to you anymore',
 				],
 				'condescending': [
-					'girl, please',
+					'[girl|sweetie], please',
+					'oh, [honey|sweetie]',
 					'yeah, no',
-					'i [literally ]could not care less',
+					'you [look|are] [cute|adorable|hot|sexy] when you are [angry|pissed off]',
+					'i [literally ]could not care less[ about you| what you think|]',
 				],
 			},
 			'introduction': {
 				'unbelievable': [
-					'how [can it be|is it] so fucking difficult to',
+					'how [can it be|is it] so [fucking ]difficult to[ just]',
+					'how [fucking ]difficult is it to[ just]',
+					'how [fucking ]often do I have to tell you to[ just]',
+					'i have [already ][repeatedly ] told you to[ just]',
+					'[how ]is it so [fucking ]difficult to[ just]',
+					'why can\'t you[ just]',
+					'why is it so [fucking ]hard for you to[ just]',
+					'you should[ really][ just]',
 				],
 			},
 			'conclusion': [
@@ -220,13 +295,6 @@
 			],
 			'sentence': [
 				'{statement.universal|uppercase-first}!',
-				// '{statement.universal}, and {statement.universal}!',
-				// '{condition} {statement.conditional}!',
-				// '{condition} {statement.conditional}, and {statement.universal}!',
-				// '{conclusion}',
-				// '{noun.trigger}',
-				// 'expected that to come from an {oppressed} {insult}',
-				// 'fighting against {concept.bad}. fighting for {concept.good}.',
 			],
 		})
 
