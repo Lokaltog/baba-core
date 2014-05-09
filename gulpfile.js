f10cf1b155c26310483a7d057a82bd0044794609
Kim Silkebækken <kim@silkebaekken.net>
2014-05-09 20:40:15 +0200

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var stylus = require('gulp-stylus')
+var nib = require('nib')
+var watchify = require('watchify')
+var streamify = require('gulp-streamify')
+var uglify = require('gulp-uglify')
+var source = require('vinyl-source-stream')
+var livereload = require('gulp-livereload')
+var es6ify = require('es6ify')
+var jade = require('gulp-jade')
+var htmlmin = require('gulp-htmlmin')
+var imagemin = require('gulp-imagemin')
+
+var __assets_src = './app/assets/'
+var __assets_dest = './webroot/static/'
+var __views_src = './app/views/'
+var __views_dest = './webroot/'
+
+gulp.task('stylus', function() {
+	return gulp
+		.src(__assets_src + 'styl/main.styl')
+		.pipe(stylus({
+			debug: false,
+			compress: true,
+			'include css': true,
+			define: {
+				ASSETS_PATH: 'static/', // required for smoosher
+			},
+			use: [
+				nib(),
+			],
+			import: [
+				'nib',
+			],
+		}))
+		.pipe(gulp.dest(__assets_dest + 'css/'))
+		.pipe(livereload())
+})
+
+gulp.task('jade', function() {
+	return gulp
+		.src(__views_src + '**.jade')
+		.pipe(jade({
+			pretty: true,
+			compileDebug: true,
+		}))
+		.pipe(htmlmin({
+			collapseBooleanAttributes: true,
+			collapseWhitespace: true,
+			removeAttributeQuotes: true,
+			removeComments: true,
+			removeEmptyAttributes: true,
+			removeOptionalTags: true,
+			removeRedundantAttributes: true,
+			useShortDoctype: true,
+		}))
+		.pipe(gulp.dest(__views_dest))
+		.pipe(livereload())
+})
+
+gulp.task('watchify', function() {
+	var bundler = watchify(__assets_src + 'js/main.js')
+	function rebundle() {
+		gutil.log('Starting watchify rebundle')
+		return bundler
+			.transform('brfs')
+			.transform(es6ify)
+			.bundle({ debug: true }, function () { gutil.log('Watchify rebundling done') })
+			.pipe(source('js/main.js'))
+			.pipe(streamify(uglify({
+				mangle: true,
+				compress: true,
+			})))
+			.pipe(gulp.dest(__assets_dest))
+			.pipe(livereload())
+	}
+	bundler.on('update', rebundle)
+	return rebundle()
+})
+
+gulp.task('sync', function() {
+	return gulp
+		.src(__assets_src + '{img,font}/**')
+		.pipe(gulp.dest(__assets_dest))
+		.pipe(livereload())
+})
+
+gulp.task('development', ['stylus', 'jade', 'watchify', 'sync'], function() {
+	gulp.watch(__assets_src + 'styl/**.styl', ['stylus'])
+	gulp.watch(__views_src + '**.jade', ['jade'])
+})
