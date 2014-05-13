e86e3d26a40a83a31bef0a5a65e11ab866c1db95
Kim Silkebækken <kim@silkebaekken.net>
2014-05-13 19:21:02 +0200

    Update gulpfile and package.json with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp')
-var gutil = require('gulp-util')
 var stylus = require('gulp-stylus')
 var nib = require('nib')
-var watchify = require('watchify')
 var streamify = require('gulp-streamify')
+var browserify = require('gulp-browserify')
 var uglify = require('gulp-uglify')
-var source = require('vinyl-source-stream')
 var livereload = require('gulp-livereload')
-var es6ify = require('es6ify')
 var jade = require('gulp-jade')
 var htmlmin = require('gulp-htmlmin')
 var imagemin = require('gulp-imagemin')
@@ -60,27 +57,19 @@ gulp.task('jade', function() {
 		.pipe(livereload())
 })
 
-gulp.task('watchify', function() {
-	var bundler = watchify(__assets_src + 'js/main.js')
-	function rebundle() {
-		gutil.log('Starting watchify rebundle')
-		return bundler
-			.transform('brfs')
-			.transform(es6ify)
-			.bundle({}, function () { gutil.log('Watchify rebundling done') })
-			.pipe(source('js/main.js'))
-			.pipe(streamify(uglify({
-				mangle: true,
-				compress: true,
-			})))
-			.pipe(gulp.dest(__assets_dest))
-			.pipe(livereload())
-	}
-	bundler.on('update', rebundle)
-	return rebundle()
+gulp.task('browserify', function() {
+	return gulp
+		.src(__assets_src + 'js/main.js')
+		.pipe(browserify({}))
+		.pipe(streamify(uglify({
+			mangle: true,
+			compress: true,
+		})))
+		.pipe(gulp.dest(__assets_dest + 'js'))
+		.pipe(livereload())
 })
 
-gulp.task('uglify-static', function() {
+gulp.task('uglify-lib', function() {
 	// libraries that don't need to be watched
 	return gulp
 		.src(__assets_src + 'js/lib/**.js')
@@ -95,7 +84,8 @@ gulp.task('sync', function() {
 		.pipe(livereload())
 })
 
-gulp.task('development', ['stylus', 'jade', 'watchify', 'uglify-static', 'sync'], function() {
+gulp.task('development', ['stylus', 'jade', 'browserify', 'uglify-lib', 'sync'], function() {
+	gulp.watch(__assets_src + 'js/*.js', ['browserify'])
 	gulp.watch(__assets_src + 'styl/**.styl', ['stylus'])
 	gulp.watch(__views_src + '**.jade', ['jade'])
 })
