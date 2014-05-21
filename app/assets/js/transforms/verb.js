400d73f6d9944b63f5d30d64babfbd199e5345ab
Kim Silkebækken <kim@silkebaekken.net>
2014-05-21 16:58:39 +0200

    Update verb conjucation rules

--- app/assets/js/transforms/verb.js
+++ app/assets/js/transforms/verb.js
@@ -18,21 +18,48 @@ module.exports = {
 					],
 				},
 				{
+					// Rules from http://www.oxforddictionaries.com/us/words/verb-tenses-adding-ed-and-ing
 					id: '8ikx0opb7j',
 					label: 'Present participle',
 					tag: '·ing',
 					type: 'suffix',
 					re: [
+						// two wovels + consonant -> not double final consonant
+						['^(.*[aeiouy]{2}[bcdfghjklmnpqrstvwxz])$', '$1ing'],
+						// words ending in c
+						['^(.*c)$', '$1king'],
+						// attempt to handle stress on verbs ending in wovel + consonant
+						// exceptions, stress on end of word
+						['^(.*)mit$', '$1mitting'],
+						['^(.*)fer$', '$1ferring'],
+						// double consonant on short (one-syllable) words
+						['^(.{1,3}[aeiou])([bdfglmnprstz])$', '$1$2$2ing'],
+						// universal rules
+						['^(.*[aeiouy]l)$', '$1ling'],
+						['^(.*?[eyo]e)$', '$1ing'],
 						['^(.*?)e?$', '$1ing'],
 					],
 				},
 				{
+					// Rules from http://www.oxforddictionaries.com/us/words/verb-tenses-adding-ed-and-ing
 					id: 'lioq5xaxsi',
 					label: 'Past',
 					tag: '·ed',
 					type: 'suffix',
 					re: [
-						['^(.*)y$', '$1ied'],
+						// two wovels + consonant -> not double final consonant
+						['^(.*[aeiouy]{2}[bcdfghjklmnpqrstvwxz])$', '$1ed'],
+						// words ending in c
+						['^(.*c)$', '$1ked'],
+						// attempt to handle stress on verbs ending in wovel + consonant
+						// exceptions, stress on end of word
+						['^(.*)mit$', '$1mitted'],
+						['^(.*)fer$', '$1ferred'],
+						// double consonant on short (one-syllable) words
+						['^(.{1,3}[aeiou])([bdfglmnprstz])$', '$1$2$2ed'],
+						// universal rules
+						['^(.*[aeiouy]l)$', '$1led'],
+						['^(.*?[eyo]e)$', '$1d'],
 						['^(.*?)e?$', '$1ed'],
 					],
 				},
